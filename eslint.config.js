436a63daa8493c9743ee4fa99634044e678b48ab
Aleh Kuis <pestleralex@gmail.com>
2025-07-15 13:27:26 +0300

    feat: setup project with eslint, prettier, husky and lint-staged

---
+++ eslint.config.js
@@ -0,0 +1,89 @@
+import typescript from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+import unicorn from "eslint-plugin-unicorn";
+import js from "@eslint/js";
+import angular from "@angular-eslint/eslint-plugin";
+import angularParser from "@angular-eslint/template-parser";
+import angularTemplatePlugin from "@angular-eslint/eslint-plugin-template";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+        sourceType: "module",
+      },
+      globals: {
+        ngDevMode: "readonly",
+        ngI18nClosureMode: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        console: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+      unicorn,
+      prettier,
+      "@angular-eslint": angular,
+    },
+    rules: {
+      // TypeScript
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+
+      // Unicorn
+      "unicorn/prevent-abbreviations": "off",
+      "unicorn/no-null": "off",
+
+      // Prettier
+      "prettier/prettier": "warn",
+
+      // JS
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      eqeqeq: ["error", "always"],
+
+      // Angular naming
+      "@angular-eslint/component-selector": [
+        "error",
+        { type: "element", prefix: "app", style: "kebab-case" },
+      ],
+      "@angular-eslint/directive-selector": [
+        "error",
+        { type: "attribute", prefix: "app", style: "camelCase" },
+      ],
+    },
+  },
+
+  {
+    files: ["**/*.html"],
+    languageOptions: {
+      parser: angularParser,
+    },
+    plugins: {
+      "@angular-eslint/template": angularTemplatePlugin,
+    },
+    rules: {
+      "@angular-eslint/template/no-negated-async": "error",
+      "@angular-eslint/template/banana-in-box": "error",
+    },
+  },
+
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.angular/**",
+      "**/dist/**",
+      "**/coverage/**",
+      "**/zone*.js",
+    ],
+  },
+];
---
+++ stylelint.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'no-empty-source': true,
+    'length-zero-no-unit': true
+  }
+};
