055dece8c7cf28630403c6341c09a65cce85525a
Aleh Kuis <pestleralex@gmail.com>
2025-07-16 15:14:34 +0300

    feat: update project structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
+import { provideZoneChangeDetection } from '@angular/core';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection()],
+}).catch((err) => console.error(err));
