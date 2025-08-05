30b6fac578ceafcf49f524f080e4c751a9b8c30d
Aleh Kuis <pestleralex@gmail.com>
2025-08-05 17:01:08 +0300

    feat: add logout

--- src/app/layout/sidebar/sidebar-footer/sidebar-footer.component.ts
+++ src/app/layout/sidebar/sidebar-footer/sidebar-footer.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { AuthService } from 'app/core/auth.service';
 
 @Component({
   selector: 'app-sidebar-footer',
   standalone: true,
   templateUrl: './sidebar-footer.component.html',
   styleUrls: ['./sidebar-footer.component.scss'],
+  imports: [MatIconModule, RouterLink],
 })
-export class SidebarFooterComponent {}
+export class SidebarFooterComponent {
+  // eslint-disable-next-line no-unused-vars
+  constructor(public auth: AuthService) {}
+
+  logout(): void {
+    this.auth.logout();
+  }
+}
