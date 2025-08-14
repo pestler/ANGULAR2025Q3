bf0072852922b761e742c4687d2c4ceba84a7210
Aleh Kuis <pestleralex@gmail.com>
2025-08-14 13:28:43 +0300

    feat: add theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/auth.service';
+import { ThemeManagerService } from './core/services/theme-manager.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ThemeSwitcherComponent } from './shared/theme-switcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, ThemeSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class App {
+export class App implements OnInit {
   private auth = inject(AuthService);
+  private themeManager = inject(ThemeManagerService);
 
   ngOnInit(): void {
     this.auth.init();
+    this.themeManager.loadTheme();
   }
 }
---
+++ src/app/core/services/theme-manager.service.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-vars */
+import { DOCUMENT } from '@angular/common';
+import {
+  Inject,
+  Injectable,
+  Renderer2,
+  RendererFactory2,
+  signal,
+} from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeManagerService {
+  isDarkMode = signal<boolean>(false);
+  private renderer: Renderer2;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private rendererFactory: RendererFactory2,
+    private overlayContainer: OverlayContainer,
+  ) {
+    this.renderer = this.rendererFactory.createRenderer(null, null);
+  }
+
+  loadTheme(): void {
+    const savedTheme = localStorage.getItem('theme');
+    const themeToSet = savedTheme === 'dark';
+    this.setTheme(themeToSet);
+  }
+
+  toggleTheme(): void {
+    this.setTheme(!this.isDarkMode());
+  }
+
+  private setTheme(isDark: boolean): void {
+    this.isDarkMode.set(isDark);
+
+    const themeClass = 'dark-theme';
+    const overlayContainerElement = this.overlayContainer.getContainerElement();
+
+    if (isDark) {
+      this.renderer.addClass(this.document.body, themeClass);
+
+      overlayContainerElement.classList.add(themeClass);
+      localStorage.setItem('home-ui-theme', 'dark');
+    } else {
+      this.renderer.removeClass(this.document.body, themeClass);
+
+      overlayContainerElement.classList.remove(themeClass);
+      localStorage.setItem('home-ui-theme', 'light');
+    }
+  }
+}
