5028fd381907c1c2945112c13b00fb8b20218809
Aleh Kuis <pestleralex@gmail.com>
2025-08-21 17:33:05 +0300

    feat: add default page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginPageComponent } from './layout/pages/login/login-page.component';
 import { authGuard } from './core/auth.guard';
 import { AboutPageComponent } from './layout/pages/about/about-page.component';
 import { NotFoundPageComponent } from './layout/pages/not-found/not-found-page.component';
+import { defaultDashboardGuard } from './core/default-dashboard.guard';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,12 @@ export const routes: Routes = [
     component: ShellComponent,
     canActivate: [authGuard],
     children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        canActivate: [defaultDashboardGuard],
+        children: [],
+      },
       {
         path: 'about',
         component: AboutPageComponent,
